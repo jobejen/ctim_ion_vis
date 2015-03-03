736b5c74cc5bfe458a624cf9166c9265626fd35f
jobejen <jbj1@wildcats.unh.edu>
2015-03-03 14:53:08 -0500

    added fort_ratio.f file to create ratio plots

---
+++ fort_ratio.f
@@ -0,0 +1,83 @@
+      program fort_ratio
+      real yes(15,91,10),noo(15,91,10),ratio(15,91,10)
+      real altyes(15,91,16),altnoo(15,91,16),altratio(15,91,16)
+
+
+      open(unit=101, file="long_slice_000yesboth.txt")
+      open(unit=102, file="long_slice_090yesboth.txt")
+      open(unit=103, file="long_slice_180yesboth.txt")
+      open(unit=104, file="long_slice_270yesboth.txt")
+      open(unit=105, file="alt_slice_k-10yes.txt")
+
+      open(unit=106, file="long_slice_000nooboth.txt")
+      open(unit=107, file="long_slice_090nooboth.txt")
+      open(unit=108, file="long_slice_180nooboth.txt")
+      open(unit=109, file="long_slice_270nooboth.txt")
+      open(unit=110, file="alt_slice_k-10noo.txt")
+
+      open(unit=111, file="long_slice_000rat.txt")
+      open(unit=112, file="long_slice_090rat.txt")
+      open(unit=113, file="long_slice_180rat.txt")
+      open(unit=114, file="long_slice_270rat.txt")
+      open(unit=115, file="alt_slice_k-10rat.txt")
+
+      do I=1,56
+      do J=1,15
+         read(101,*) yes(J,I,1),yes(J,I,2),yes(J,I,3),yes(J,I,4),
+     1yes(J,I,5),yes(J,I,6),yes(J,I,7),yes(J,I,8),yes(J,I,9),yes(J,I,10)
+      enddo
+c      read(101,*)dummy
+      enddo
+
+      do I=1,56
+      do J=1,15
+         read(106,*) noo(J,I,1),noo(J,I,2),noo(J,I,3),noo(J,I,4),
+     1noo(J,I,5),noo(J,I,6),noo(J,I,7),noo(J,I,8),noo(J,I,9),noo(J,I,10)
+      enddo
+      enddo
+
+      do I=1,56
+      do J=1,15
+      write(111,*) noo(J,I,1),noo(J,I,2),noo(J,I,3),
+     1noo(J,I,4),yes(J,I,5)/noo(J,I,5),yes(J,I,6)/noo(J,I,6),
+     2yes(J,I,7)/noo(J,I,7),yes(J,I,8)/noo(J,I,8),yes(J,I,9)-noo(J,I,9),
+     3yes(J,I,10)-noo(J,I,10)
+      enddo
+      write(111,*)" "
+      enddo
+
+      do I=1,56
+      do J=1,15
+         read(105,*) altyes(J,I,1),altyes(J,I,2),altyes(J,I,3),
+     1altyes(J,I,4),altyes(J,I,5),altyes(J,I,6),altyes(J,I,7),
+     2altyes(J,I,8),altyes(J,I,9),altyes(J,I,10),altyes(J,I,11),
+     3altyes(J,I,12),altyes(J,I,13),altyes(J,I,14),altyes(J,I,15)
+      enddo
+      enddo
+
+      do I=1,56
+      do J=1,15
+         read(110,*) altnoo(J,I,1),altnoo(J,I,2),altnoo(J,I,3),
+     1altnoo(J,I,4),altnoo(J,I,5),altnoo(J,I,6),altnoo(J,I,7),
+     2altnoo(J,I,8),altnoo(J,I,9),altnoo(J,I,10),altnoo(J,I,11),
+     3altnoo(J,I,12),altnoo(J,I,13),altnoo(J,I,14),altnoo(J,I,15)
+      enddo
+      enddo
+
+      do I=1,56
+      do J=1,15
+        write(115,*) altyes(J,I,1),altyes(J,I,2),altyes(J,I,3),
+     1altyes(J,I,4),altyes(J,I,5),altyes(J,I,6),
+     2altyes(J,I,7)/altnoo(J,I,7),altyes(J,I,8)/altnoo(J,I,8),
+     3altyes(J,I,9)/altnoo(J,I,9),altyes(J,I,10)/altnoo(J,I,10),
+     4altyes(J,I,11)/altnoo(J,I,11),altyes(J,I,12)/altnoo(J,I,12),
+     5altyes(J,I,13)/altnoo(J,I,13),altyes(J,I,14)/altnoo(J,I,14),
+     6altyes(J,I,15)/altnoo(J,I,15)
+      enddo
+      write(115,*)" "
+      enddo
+
+
+
+
+      end program
